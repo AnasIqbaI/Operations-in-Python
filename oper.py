473e63a41e04f38511126b821cfd8f78957008b1
AnasIqbaI <arafdarky@gmail.com>
2025-07-13 23:15:39 +0600

    Add files via upload

---
+++ oper.py
@@ -0,0 +1,34 @@
+English = 50
+Science = 60
+Math = 70 
+
+def calculate_total(English, Science, Math):
+    total = English + Science + Math
+    return total
+
+print("Total Marks:", calculate_total(English, Science, Math))
+
+num1 = 45
+num2 = 3
+
+print("Number 1 ", num1)
+print("Number 2 ", num2)
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Modulus:", num1 % num2)
+print("Exponentiation:", num1 ** num2)
+print("Difference :", num1 - num2)
+print("Square :", num1 * num1)
+print("Square Root :", num1** 0.5)
+
+print("Equal ?", num1 == num2)
+print("Number 1 greater?", num1 > num2)
+print("Number 2 greater?", num1 < num2)
+print("Not Equal ?", num1 != num2)
+
+
+result = num1/2+ num2**2+10
+print("Result of expression:", result)
